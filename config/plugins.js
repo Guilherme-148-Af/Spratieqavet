850ba5debcf302e24e5af23d91a1a11c463dac78
Guilherme <guilherme148afonso@gmail.com>
2026-02-05 16:18:11 +0000

    Configure S3 upload provider for persistent storage

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,18 @@
-module.exports = () => ({});
+﻿module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('S3_ACCESS_KEY_ID'),
+        secretAccessKey: env('S3_ACCESS_SECRET'),
+        region: env('S3_REGION', 'auto'),
+        params: {
+          Bucket: env('S3_BUCKET'),
+        },
+        // Para Cloudflare R2 (S3-compatible)
+        endpoint: env('S3_ENDPOINT'),
+        s3ForcePathStyle: true,
+      },
+    },
+  },
+});
